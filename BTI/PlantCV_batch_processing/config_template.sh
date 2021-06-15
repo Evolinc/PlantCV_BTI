9be8ae99cb60cc0ffb649b4b117f4b268bc068e2
Li'ang Yu <69836931+Leon-Yu0320@users.noreply.github.com>
2021-06-15 10:58:44 -0400

    Update config_template.sh

--- BTI/PlantCV_batch_processing/config_template.sh
+++ BTI/PlantCV_batch_processing/config_template.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-#specify each directory for each argument required
-pipe_dir="directory/to/pipelines" 
-# under the directory: multi.py, s2_multiple_batcg.sh, and json file should be stored
+#directory related to argument
+pipe_dir="/home/liangyu/1_Project/2_PlantCV/2_scripts"
 
-raw_dir="directory/to/raspi_images/been/collected"
+echo"**********The following scripts are required under the pipeline directory !***************" under the pipe_dir directory: 
+echo"1.multi_plant.py"
+echo"2.multi_plant.json"
+echo"3.s1_Image_distribution.sh"
+echo"4.s2_multiple_batch.sh"
+echo"5.s3_Rig_process.R"
 
-#specify directory for all arguments
+raw_dir="/home/liangyu/0_data/3_Raspi"
+parameter_code_dir="/home/liangyu/1_Project/2_PlantCV/5_Project/2_LncRNA_20201030/parameter_code"
+master_input_dir="/home/liangyu/1_Project/2_PlantCV/5_Project/2_LncRNA_20201030"
+master_json_dir="/home/liangyu/1_Project/2_PlantCV/5_Project/2_LncRNA_20201030/Results"
+master_output_dir="/home/liangyu/1_Project/2_PlantCV/5_Project/2_LncRNA_20201030/Results"
 
-parameter_code_dir="directory/to/parameter_code"
-master_input_dir="directory/to/input_images"
-master_json_dir="directory/to/json_outputs"
-master_output_dir="directory/to/image(png)_outputs"
-
-#allocate all images to certian directory
+# STEP 1 allocate all images to certian directory
 bash $pipe_dir/s1_image_distribution_previous_stamp.sh -l $master_input_dir/photo_list -r $raw_dir -p $master_input_dir -t $master_input_dir/date_list
 
-#perform batch processing for each selected images
-bash $pipe_dir/s2_multiple_batch.sh -p $pipe_dir -c $parameter_code_dir -i $master_input_dir -j $master_json_dir -o $master_output_dir -s $master_input_dir/meta.csv
+#STEP 2 perform batch processing for each selected images
+bash $pipe_dir/s2_multiple_batch.sh -p $pipe_dir -c $parameter_code_dir -i $master_input_dir -j $master_json_dir -o $master_output_dir -s $master_input_dir/LncRNA_20201030_meta.csv
+
+#STEP 3 perform time stamp transformation for each camera
+ls $master_json_dir/*.clean.csv | grep -v "sample.list" > $master_json_dir/sample.list
+
+for i in $(cat $master_json_dir/sample.list);do
+	Rscript $pipe_dir/s3_Rig_process.R --input $master_json_dir/${i} --output $master_json_dir/${i}
+done
+
